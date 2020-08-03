82f9888020ca7fc8b082ef45b6fc4d0bef2d2d80
Pablo Vazquez <pvazquez@vandldigital.com>
2020-08-03 01:09:04 -0400

    swapi api fetch data function, and error handler

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import errorHandler from './fetchErrorHandler';
 
 export default {
     fetchAllData: async (cb)=> {
@@ -12,7 +13,7 @@ export default {
                 axios.get('https://swapi.dev/api/vehicles'),
             ]);
 
-            const filteredData = data.map((data)=>{
+            const mappedData = data.map((data)=>{
                 return {
                     title: data.data.results[0].name,
                     category: data.config.url.split('/')[4],
@@ -20,18 +21,50 @@ export default {
                 };
             });
 
-            cb(filteredData);
+            cb(mappedData);
 
         }catch(error){
-            console.log(error);
+            errorHandler.fetchAllDataFallback(error);
         }
     },
     fetchPeopleData: async (cb)=> {
         try{
-            const data = await axios.get('https://swapi.dev/api/people');
-            cb(data.data);
+            const people = await axios.get('https://swapi.dev/api/people');
+            cb(people.data);
         }catch(error){
-            console.log(error);
+            errorHandler.fetchPeopleDataFallback(error);
         }
-    },  
+    },
+    fetchPlanetsData: async (cb)=> {
+        try{
+            const planets = await axios.get('http://swapi.dev/api/planets');
+            cb(planets.data);
+        }catch(error){
+            errorHandler.fetchPlanetsDataFallback(error);
+        }
+    },
+    fetchSpeciesData: async (cb)=> {
+        try{
+            const species = await axios.get('http://swapi.dev/api/species');
+            cb(species.data);
+        }catch(error){
+            errorHandler.fetchSpeciesDataFallback(error);
+        }
+    },
+    fetchStarshipsData: async (cb)=> {
+        try{
+            const starships = await axios.get('http://swapi.dev/api/starships');
+            cb(starships.data);
+        }catch(error){
+            errorHandler.fetchStarshipsDataFallback(error);
+        }
+    },
+    fetchVehiclesData: async (cb)=> {
+        try{
+            const vehicles = await axios.get('http://swapi.dev/api/vehicles');
+            cb(vehicles.data);
+        }catch(error){
+            errorHandler.fetchVehiclesDataFallback(error);
+        }
+    },
 }
---
+++ src/utils/fetchErrorHandler.js
@@ -0,0 +1,17 @@
+export default {
+    fetchAllDataFallback: (error)=> {
+        console.log(error);
+    },
+    fetchPeopleDataFallback: (error)=> {
+        console.log(error);
+    },
+    fetchPlanetsDataFallback: (error)=> {
+        console.log(error);
+    },
+    fetchStarshipsDataFallback: (error)=> {
+        console.log(error);
+    },
+    fetchVehiclesDataFallback: (error)=> {
+        console.log(error);
+    }
+}
