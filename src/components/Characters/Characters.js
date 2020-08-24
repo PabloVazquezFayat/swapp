873462197092c126fe6bd973186cbe2104db3f1b
Pablo Vazquez <pvazquez@vandldigital.com>
2020-08-24 14:51:40 -0400

    input validation and duplicate prevenation

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import fetchData from '../../utils/fetchData'
+import Alert from '../Alert/Alert';
 
 export default function Characters() {
 
     const [data, setData] = useState('');
     const [propertyData, setPropertyData] = useState({});
+    const [alert, setAlert] = useState({status: false, newValue: false});
 
     const getData = async ()=> {
         if(data === ''){
@@ -20,26 +22,37 @@ export default function Characters() {
     const editPropery = async ()=> {
 
         const newData = {...data};
-        newData.name = propertyData.name;
-        console.log(newData);
 
-        const res = await fetchData.updateCharacterData(newData);
-        console.log(res);
-        setData(res);
-        
-        console.log(data);
+        if(
+            Object.keys(propertyData).length !== 0 && 
+            propertyData[Object.keys(propertyData)[0]] !== undefined && 
+            propertyData[Object.keys(propertyData)[0]].length > 0 &&
+            propertyData[Object.keys(propertyData)[0]] !== newData[Object.keys(propertyData)[0]]
+        ){
 
-        // if(data[Object.keys(propertyData)] !== undefined){
-        //     data[Object.keys(propertyData)[0]] = propertyData[Object.keys(propertyData)];
-        //     console.log(data);
-        //     data.id = data._id;
-        //     delete data._id;
-        //     const res = await fetchData.updateCharacterData(data);
-        //     setData(res);
-        //     setPropertyData({});
-        // }else{
-        //     console.log('nothing to save')
-        // }
+            newData[Object.keys(propertyData)[0]] = propertyData[Object.keys(propertyData)];
+
+            const res = await fetchData.updateCharacterData(newData);
+
+            setData(res);
+
+            setAlert({status: false, newValue: false});
+
+        }else if(
+            Object.keys(propertyData).length !== 0 && 
+            propertyData[Object.keys(propertyData)[0]] !== undefined && 
+            propertyData[Object.keys(propertyData)[0]].length > 0 &&
+            propertyData[Object.keys(propertyData)[0]] === newData[Object.keys(propertyData)[0]]
+        ){
+
+            setAlert({status: true, newValue: false});
+
+        }else{
+
+            setAlert({status: true, newValue: true});
+
+        }
+
     }
 
     const createCharater = ()=> {
@@ -49,12 +62,9 @@ export default function Characters() {
                             <p>{data.name}</p>
                             <input type='text' onChange={(e)=> setPropertyData({name: e.target.value})} placeholder={data.name}></input>
                             <button onClick={editPropery}>save</button>
+                            {alert.status === true && alert.newValue === true? <Alert message={`please enter a valid value`}/> : null}
+                            {alert.status === true && alert.newValue === false ? <Alert message={`please enter a NEW value`}/> : null}
                         </div>
-
-                        <div>
-                            
-                        </div>
-
                         <img src={data.imageURL ? data.imageURL : '/imgs/man.png' } alt=''></img>
                     </div>
         }else if(data.status === false){
