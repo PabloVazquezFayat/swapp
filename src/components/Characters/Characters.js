8dcbc583f001274145a4c52ce51dee4c7731cb8b
Pablo Vazquez <pvazquez@vandldigital.com>
2020-08-24 16:41:31 -0400

    reusable Input component

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import fetchData from '../../utils/fetchData'
-import Alert from '../Alert/Alert';
+import Input from '../Input/Input'
 
 export default function Characters() {
 
     const [data, setData] = useState('');
-    const [propertyData, setPropertyData] = useState({});
-    const [alert, setAlert] = useState({status: false, newValue: false});
 
     const getData = async ()=> {
         if(data === ''){
@@ -19,43 +17,10 @@ export default function Characters() {
         getData();
     }, []);
 
-    const editPropery = async ()=> {
-
-        const newData = {...data};
-
-        if(
-            Object.keys(propertyData).length !== 0 && 
-            propertyData[Object.keys(propertyData)[0]] !== undefined && 
-            propertyData[Object.keys(propertyData)[0]].length > 0 &&
-            propertyData[Object.keys(propertyData)[0]] !== newData[Object.keys(propertyData)[0]]
-        ){
-            newData[Object.keys(propertyData)[0]] = propertyData[Object.keys(propertyData)];
-            const res = await fetchData.updateCharacterData(newData);
-            setData(res);
-            setAlert({status: false, newValue: false});
-        }else if(
-            Object.keys(propertyData).length !== 0 && 
-            propertyData[Object.keys(propertyData)[0]] !== undefined && 
-            propertyData[Object.keys(propertyData)[0]].length > 0 &&
-            propertyData[Object.keys(propertyData)[0]] === newData[Object.keys(propertyData)[0]]
-        ){
-            setAlert({status: true, newValue: false});
-        }else{
-            setAlert({status: true, newValue: true});
-        }
-
-    }
-
     const createCharater = ()=> {
         if(Object.keys(data).length > 0 && data.status === undefined){
             return  <div>
-                        <div>
-                            <p>{data.name}</p>
-                            <input type='text' onChange={(e)=> setPropertyData({name: e.target.value})} placeholder={data.name}></input>
-                            <button onClick={editPropery}>save</button>
-                            {alert.status === true && alert.newValue === true? <Alert message={`please enter a valid value`}/> : null}
-                            {alert.status === true && alert.newValue === false ? <Alert message={`please enter a NEW value`}/> : null}
-                        </div>
+                        <Input data={{...data}} cb={setData} propertyName="name"/>
                         <img src={data.imageURL ? data.imageURL : '/imgs/man.png' } alt=''></img>
                     </div>
         }else if(data.status === false){
---
+++ src/components/Input/Input.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import Alert from '../Alert/Alert'
+import fetchData from '../../utils/fetchData'
+
+export default function Input(props) {
+
+    const [propertyData, setPropertyData] = useState({});
+    const [alert, setAlert] = useState({status: false, newValue: false});
+
+    const editPropery = async ()=> {
+
+        const newData = props.data;
+
+        if(
+            Object.keys(propertyData).length !== 0 && 
+            propertyData[Object.keys(propertyData)[0]] !== undefined && 
+            propertyData[Object.keys(propertyData)[0]].length > 0 &&
+            propertyData[Object.keys(propertyData)[0]] !== newData[Object.keys(propertyData)[0]]
+        ){
+            newData[Object.keys(propertyData)[0]] = propertyData[Object.keys(propertyData)];
+            const res = await fetchData.updateCharacterData(newData);
+            props.cb(res);
+            setAlert({status: false, newValue: false});
+        }else if(
+            Object.keys(propertyData).length !== 0 && 
+            propertyData[Object.keys(propertyData)[0]] !== undefined && 
+            propertyData[Object.keys(propertyData)[0]].length > 0 &&
+            propertyData[Object.keys(propertyData)[0]] === newData[Object.keys(propertyData)[0]]
+        ){
+            setAlert({status: true, newValue: false});
+        }else{
+            setAlert({status: true, newValue: true});
+        }
+
+    }
+
+    return (
+        <div>
+            <p>{props.data.name}</p>
+            <input type='text' onChange={(e)=> setPropertyData({[props.propertyName]: e.target.value})} placeholder={props.data.name}></input>
+            <button onClick={editPropery}>save</button>
+            {alert.status === true && alert.newValue === true? <Alert message={`please enter a valid value`}/> : null}
+            {alert.status === true && alert.newValue === false ? <Alert message={`please enter a NEW value`}/> : null}
+        </div>
+    )
+}
