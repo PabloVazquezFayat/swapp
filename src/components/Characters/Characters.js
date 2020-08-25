6546c0a671509b3c708a58b9632f0685c7c68cb6
Pablo Vazquez <pvazquez@vandldigital.com>
2020-08-25 18:48:11 -0400

    first draft at reusable select drop down component

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import fetchData from '../../utils/fetchData'
 import Input from '../Input/Input'
+import InputSelect from '../InputSelect/InputSelect'
 
 export default function Characters() {
 
@@ -8,8 +9,8 @@ export default function Characters() {
 
     const getData = async ()=> {
         if(data === ''){
-            const results = await fetchData.fetchCharacterDataByID(window.location.href.split('/')[4]);
-            setData(results)
+            const characterData = await fetchData.fetchCharacterDataByID(window.location.href.split('/')[4]);
+            setData(characterData);
         }  
     }
 
@@ -22,6 +23,10 @@ export default function Characters() {
             return  <div>
                         <p>{data.name}</p>
                         <Input data={{...data}} cb={setData} propertyName="name" endpoint="updateCharacterData"/>
+
+                        <p>{data.homeWorld.name ? data.homeWorld.name : 'Homeworld unknown'}</p>
+                        <InputSelect data={{...data}} cb={setData} propertyName="homeWorld" endpoints={{options: 'fetchPlanetsData', update: 'updateCharacterData'}}/>
+
                         <img src={data.imageURL ? data.imageURL : '/imgs/man.png' } alt=''></img>
                     </div>
         }else if(data.status === false){
---
+++ src/components/InputSelect/InputSelect.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import fetchData from '../../utils/fetchData'
+
+export default function InputSelect(props) {
+
+    console.log(props.data);
+
+    const [options, setOptions] = useState('');
+    const [propertyData, setPropertyData] = useState({});
+
+    const getData = async ()=> {
+        if(options === ''){
+            const response = await fetchData[props.endpoints.options]();
+            setOptions(response);
+        }
+    }
+
+    useEffect(()=> {
+        getData();
+    }, []);
+
+    const createInputSelect = ()=> {
+        if(options){
+            return  options.map((opt, i)=> {
+                return <option key={i} value={opt._id}>{opt.name}</option>
+            })
+        }else{
+            return <option>Loading...</option>
+        }
+    }
+
+    const editProperty = async ()=> {
+
+        const newData = {...props.data};
+        
+        if(propertyData[props.propertyName] !== undefined && propertyData[props.propertyName] !== newData[props.propertyName]){
+            newData[props.propertyName] = propertyData[props.propertyName];
+            const response = await fetchData[props.endpoints.update](newData);
+            props.cb(response);
+        }
+    }
+
+    return (
+        <div>
+            <select onChange={(e)=> setPropertyData({[props.propertyName]: e.target.value})}>
+                <option>Select Planet</option>
+                {createInputSelect()}
+            </select>
+            <button onClick={editProperty}>save</button>
+        </div>
+    )
+}
