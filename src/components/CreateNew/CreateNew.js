5102c679a599a60974a4d1b68d8b76338a63bc62
Pablo Vazquez <pvazquez@vandldigital.com>
2020-09-03 17:58:28 -0400

    CreateNew component WIP

--- src/components/CreateNew/CreateNew.js
+++ src/components/CreateNew/CreateNew.js
@@ -1,10 +1,5 @@
 import React, {useState, useEffect} from 'react'
 import fetchData from '../../utils/fetchData'
-import Input from '../Input/Input'
-import InputLabel from '../InputLabel/InputLabel'
-import InputSelectMultipleLabel from '../InputSelectMultiple/InputSelectMultiple'
-import InputSelect from '../InputSelect/InputSelect'
-import InputSelectMultiple from '../InputSelectMultiple'
 
 export default function CreateNew(props) {
 
@@ -13,13 +8,16 @@ export default function CreateNew(props) {
     const [checks, setChecks] = useState('');
 
     const createInputs = ()=> {
-        // this will need a property name
         if(props.inputs && props.inputs.length > 0){
             return  props.inputs.map((input, i)=> {
-                return  <div>
-                            <InputLabel propertyName={input.propertyName}>{input.propertyName}</InputLabel>
-                            <Input data={{...data}} cb={setData} propertyName="name" endpoint="updateCharacterData"/>
-                        </div>
+                return  <li key={i}>
+                            <label>{input.propertyName}</label>
+                            <input type="text" placeholder={input.propertyName} onChange={(e)=> {
+                                let newData = {...data};
+                                newData[input.propertyName] = e.target.value;
+                                setData(newData);
+                            }} />
+                        </li>
             })
         }
     }
@@ -60,6 +58,7 @@ export default function CreateNew(props) {
 
     return (
         <div>
+            <ul>{createInputs()}</ul>
             
         </div>
     )
